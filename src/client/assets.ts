384ad633f3d41c28995dc46ca19af2b55342034e
NovaSagittarii <novasagittarii@gmail.com>
2023-11-25 00:51:28 -0800

    use imports instead of hardcoded path

--- src/client/assets.ts
+++ src/client/assets.ts
@@ -1,4 +1,15 @@
-const prefix = "src/assets/";
+import f1 from '../assets/f1.png';
+import f2 from '../assets/f2.png';
+import f3 from '../assets/f3.png';
+import f4 from '../assets/f4.png';
+import f5 from '../assets/f5.png';
+import f6 from '../assets/f6.png';
+import f7 from '../assets/f7.png';
+import f8 from '../assets/f8.png';
+import f9 from '../assets/f9.png';
+import f10 from '../assets/f10.png';
+import f11 from '../assets/f11.png';
+
 type FruitData = {
   /**
    * relative path from /src/client/* of location image
@@ -9,7 +20,7 @@ type FruitData = {
    * center of sprite
    */
   anchor: [number, number];
-  
+
   /**
    * radius of sprite to match hitbox
    */
@@ -17,57 +28,57 @@ type FruitData = {
 };
 export const FRUIT_DATA: FruitData[] = [
   {
-    path: prefix + "f1.png",
+    path: f1,
     anchor: [0.5, 0.5],
     radius: 0.5,
   },
   {
-    path: prefix + "f2.png",
+    path: f2,
     anchor: [0.5, 0.5],
     radius: 0.6,
   },
   {
-    path: prefix + "f3.png",
+    path: f3,
     anchor: [0.5, 0.55],
     radius: 0.55,
   },
   {
-    path: prefix + "f4.png",
+    path: f4,
     anchor: [0.5, 0.5],
     radius: 0.5,
   },
   {
-    path: prefix + "f5.png",
+    path: f5,
     anchor: [0.5, 0.5],
     radius: 0.5,
   },
   {
-    path: prefix + "f6.png",
+    path: f6,
     anchor: [0.5, 0.53],
     radius: 0.52,
   },
   {
-    path: prefix + "f7.png",
+    path: f7,
     anchor: [0.5, 0.7],
     radius: 0.7,
   },
   {
-    path: prefix + "f8.png",
+    path: f8,
     anchor: [0.5, 0.5],
     radius: 0.52,
   },
   {
-    path: prefix + "f9.png",
+    path: f9,
     anchor: [0.5, 0.6],
     radius: 0.6,
   },
   {
-    path: prefix + "f10.png",
+    path: f10,
     anchor: [0.5, 0.5],
     radius: 0.51,
   },
   {
-    path: prefix + "f11.png",
+    path: f11,
     anchor: [0.5, 0.5],
     radius: 0.5,
   },
