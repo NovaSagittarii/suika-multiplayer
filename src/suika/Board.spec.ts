c8f2085eba4548dc60eea2f01b0f5eb656edd0ae
NovaSagittarii <novasagittarii@gmail.com>
2024-06-12 00:57:50 -0600

    initialize walls and test ball collision

--- src/suika/Board.spec.ts
+++ src/suika/Board.spec.ts
@@ -1,6 +1,8 @@
-import Rapier from '@/lib/RapierInstance';
 import { expect, test } from 'vitest';
+
+import Rapier from '@/lib/RapierInstance';
 import Ball from '@/suika/Ball';
+import Board from '@/suika/Board';
 
 // Note: normal rapier doesn't really work with vitest.
 // you need rapier-compat so it works
@@ -13,4 +15,22 @@ test('balls fall due to gravity', () => {
   world.step();
   expect(ball.translation().x).toBe(0);
   expect(ball.translation().y).not.toBe(0);
+});
+
+test('balls fall and then fall to opposite sides', () => {
+  const board = new Board();
+  const a = board.createBall(0, 10, 0);
+  const b = board.createBall(0.05, 8, 0);
+  expect(a.translation().x).toBe(0);
+  expect(b.translation().x).toBeCloseTo(0.05);
+
+  // This assumes the balls will have fallen and collided within 100 time steps.
+  for (let i = 0; i < 100; ++i) board.step();
+  // for (let i = 0; i < 1000; ++i) {
+  //   if (i % 100 === 0) console.log(a.translation().x, b.translation().x);
+  //   board.step();
+  // }
+
+  expect(a.translation().x).toBeLessThan(0);
+  expect(b.translation().x).toBeGreaterThan(0.1);
 });
--- src/suika/Board.ts
+++ src/suika/Board.ts
@@ -1,13 +1,34 @@
 import RAPIER from '@dimforge/rapier2d-compat/rapier';
 
+import { BOARD_GRAVITY, BOARD_HEIGHT, BOARD_WIDTH } from '@/constants';
 import Rapier from '@/lib/RapierInstance';
+import Ball from '@/suika/Ball';
+import Wall from '@/suika/Wall';
 
+interface BoardConfiguration {
+  width?: number;
+  height?: number;
+}
+
+/**
+ * Represents the board (container) and balls, handles the physics.
+ */
 class Board {
   private world: RAPIER.World;
-  constructor() {
-    this.world = new Rapier.World(new Rapier.Vector2(0, -9.81));
+  private balls: Ball[] = [];
+  public readonly walls: Wall[];
+  constructor({
+    width = BOARD_WIDTH,
+    height = BOARD_HEIGHT,
+  }: BoardConfiguration = {}) {
+    this.world = new Rapier.World(new Rapier.Vector2(0, -BOARD_GRAVITY));
 
     // setup walls
+    this.walls = [
+      new Wall(this.world, -width / 2 - 1, 0, 1, height * 2),
+      new Wall(this.world, width / 2 + 1, 0, 1, height * 2),
+      new Wall(this.world, 0, -height - 1, width, 1),
+    ];
   }
 
   /**
@@ -15,6 +36,26 @@ class Board {
    */
   public free() {
     this.world.free();
+  }
+
+  /**
+   * Creates a ball of a specific type at a fixed location.
+   * @param x position of ball
+   * @param y position of ball
+   * @param ballType type of ball (indexes into the radius list)
+   * @returns ball that was just created
+   */
+  public createBall(x: number, y: number, ballType: number) {
+    const ball = new Ball(this.world, x, y, ballType);
+    this.balls.push(ball);
+    return ball;
+  }
+
+  /**
+   * Advance the board simulation by one time step.
+   */
+  public step() {
+    this.world.step();
   }
 }
 
