0fb5c741254790a7d74234b241b7e22ca82e63b6
NovaSagittarii <novasagittarii@gmail.com>
2024-06-14 02:59:48 -0600

    use withServer instead of pasted wss/server setup and cleanup

--- src/server/SuikaMultiplayerServer.spec.ts
+++ src/server/SuikaMultiplayerServer.spec.ts
@@ -26,26 +26,43 @@ class WebSocketMock extends EventEmitter {
   }
 }
 
-test('Server startup and shutdown', () => {
+/**
+ * Sets up a mock WebSocketServer and SuikaMultiplayerServer to use,
+ * then cleans up afterwards.
+ * @param cb callback to use the wss/server
+ */
+function withServer(
+  cb: (wss: WebSocketServer, server: SuikaMultiplayerServer) => void,
+) {
   const wss = new WebSocketServerMock();
   const server = new SuikaMultiplayerServer();
   server.run(wss);
+  cb(wss, server);
   server.shutdown();
+}
+
+test('Server startup and shutdown', () => {
+  withServer(() => {});
   // idk how to detect interval stoppage ??
 });
 
 test('Many joins and leaves -- fuzzing for race condition', () => {
-  const wss = new WebSocketServerMock();
-  const server = new SuikaMultiplayerServer();
-  server.run(wss);
-
-  const wsList = new Array(100).fill(0).map(() => new WebSocketMock());
-  for (const ws of shuffled(wsList)) {
-    wss.emit('connection', ws, { socket: { remoteAddress: 'fake_addr' } });
-  }
-  for (const ws of shuffled(wsList)) {
-    ws.close();
-  }
-
-  server.shutdown();
+  withServer((wss, server) => {
+    const wsList = new Array(100).fill(0).map(() => new WebSocketMock());
+    for (const ws of shuffled(wsList)) {
+      wss.emit('connection', ws, { socket: { remoteAddress: 'fake_addr' } });
+    }
+    for (const ws of shuffled(wsList)) {
+      ws.close();
+    }
+  });
 });
+
+// test('Garbage cannot be sent to self -- 1 player', () => {});
+// test('Garbage cannot be sent to self -- 2 players', () => {});
+// test('Garbage cannot be sent to self -- 6 players', () => {});
+// test('Garbage cannot be sent to dead players', () => {});
+
+// something about afk check
+// test('Disconnect player after 25 frames of inactivity', () => {});
+// maybe reset after 25 frames global inactivity ??
